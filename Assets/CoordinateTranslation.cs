05acfaa998112e45c3c0dc437517f93151c1d8bf
BuildTools <jtyler_@outlook.com>
2016-10-31 17:52:59 -0500

    Added ASC import utility. Imported Fargo g06665194

---
+++ Assets/CoordinateTranslation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoordinateTranslation : MonoBehaviour
+{
+
+    private Vector2 actualLowerLeftCorner;
+    public Vector2 ActualLowerLeftCorner { get { return actualLowerLeftCorner; } set { actualLowerLeftCorner = value; } }
+    /*
+    public Vector2 ActualUpperLeftCorner { }
+    public Vector2 ActualLowerRightCorner { }
+    public Vector2 ActualUpperRightCorner { }
+    */
+}
