8be9400254c0025f8a5008141847009d3192e588
Joseph Mitchell <jmitch930@gmail.com>
2016-12-03 16:45:12 -0600

    Added quit button when pressing left top menu button

---
+++ Assets/Scripts/ButtonCollision.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonCollision : MonoBehaviour {
+
+    private UnityEngine.UI.Image rend;
+    public Color defaultColor, HighlightedColor;
+    private bool isHighlighted = false;
+
+    void Start ()
+    {
+        rend = GetComponent<UnityEngine.UI.Image>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        rend.color = HighlightedColor;
+        isHighlighted = true;
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        rend.color = defaultColor;
+        isHighlighted = false;
+    }
+
+    public bool IsHighlighted()
+    {
+        return isHighlighted;
+    }
+}
