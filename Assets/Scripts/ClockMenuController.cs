d2fc71e35eda73328bf417f3e7c990031a186f99
Joseph Mitchell <jmitch930@gmail.com>
2016-12-03 12:53:27 -0600

    Added Time Menu, disabled rapid time change

---
+++ Assets/Scripts/ClockMenuController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClockMenuController : MonoBehaviour {
+
+    public Canvas clockCanvas;
+    private bool isMenuOpen = false;
+
+    void OnEnable()
+    {
+        LeftMenuController.OnMenuSelection += EnableClockMenu;
+    }
+
+    void OnDisable()
+    {
+        LeftMenuController.OnMenuSelection -= EnableClockMenu;
+    }
+
+    public void EnableClockMenu(LeftMenuController.MenuOption selection)
+    {
+        if(selection == LeftMenuController.MenuOption.Mag_Selected)
+        {
+        }
+
+        else if (selection != LeftMenuController.MenuOption.Sun_Selected)
+        {
+            if(isMenuOpen)
+            {
+                clockCanvas.gameObject.SetActive(false);
+                isMenuOpen = false;
+            }          
+        }
+
+        else if (!isMenuOpen)
+        {
+            clockCanvas.gameObject.SetActive(true);
+            isMenuOpen = !isMenuOpen;
+        }
+        else
+        {
+            clockCanvas.gameObject.SetActive(false);
+            isMenuOpen = !isMenuOpen;
+        }
+    }
+}
